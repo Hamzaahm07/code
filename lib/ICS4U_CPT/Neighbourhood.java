cb3efc54283c7fac9034b84c64ef987b71d24ae4
Hamza <354062507@educ.dpcdsb.org>
2025-01-16 23:09:18 -0500

    Successfully coded and checked for errors in my neighbourhood class file

--- lib/ICS4U_CPT/Neighbourhood.java
+++ lib/ICS4U_CPT/Neighbourhood.java
@@ -1,5 +1,37 @@
 package lib.ICS4U_CPT;
 
 public class Neighbourhood {
-    
+    private String name;
+    private double crimeRate; // Per 1,000 people
+    private double averageHomePrice; // In CAD
+    private int transitAccessibility; // Rating out of 10
+
+    public Neighbourhood(String name, double crimeRate, double averageHomePrice, int transitAccessibility) {
+        this.name = name;
+        this.crimeRate = crimeRate;
+        this.averageHomePrice = averageHomePrice;
+        this.transitAccessibility = transitAccessibility;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getCrimeRate() {
+        return crimeRate;
+    }
+
+    public double getAverageHomePrice() {
+        return averageHomePrice;
+    }
+
+    public int getTransitAccessibility() {
+        return transitAccessibility;
+    }
+
+    @Override
+    public String toString() {
+        return name + " - Crime Rate: " + crimeRate + "/1000, Average Home Price: $" +
+                averageHomePrice + ", Transit Accessibility: " + transitAccessibility + "/10";
+    }
 }
